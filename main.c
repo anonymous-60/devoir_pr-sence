46c3488b22cdd83904a735944b283ffc8518d441
anonymous-60 <a42970833@gmail.com>
2024-07-26 13:58:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 int main()
 {
 	char menu[MAX_SIZE];
-	FILE* file;
+	FILE* file=NULL;
 	student mit[76];
 	char line[MAX_SIZE];
 	char retour[MAX_SIZE];
 	char choice[MAX_SIZE];
-	char ret[MAX_SIZE];
 	attendance_fonction(file,line,retour,menu,mit,choice);
 	return 0;
 }
